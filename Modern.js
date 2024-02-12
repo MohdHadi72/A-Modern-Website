1265f414fb5dd45be66485b058493945ee3104c0
Mohd Hadi <154020781+MohdHadi72@users.noreply.github.com>
2024-02-12 12:53:23 +0530

    Add files via upload

---
+++ Modern.js
@@ -0,0 +1,9 @@
+let Btn = document.getElementById('Btn');
+let light = document.getElementById('light');
+
+
+function toggleBtn() {
+  Btn.classList.toggle("active");
+  light.classList.toggle("on");
+
+}
